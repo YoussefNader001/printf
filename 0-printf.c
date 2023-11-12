d579f17bb4db0856ccb9ffdae54596290073cc90
mariiamemad <mariiammemad@gmail.com>
2023-11-12 10:26:38 -0800

    k

--- 0-printf.c
+++ 0-printf.c
@@ -1,61 +1,51 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
 
-int _printf(const char *format, ...) {
+/**
+ * _printf - Produces output according to a format.
+ * @format: Format string with conversion specifiers.
+ *
+ * Return: The number of characters printed (excluding the null byte).
+ */
+int _printf(const char *format, ...)
+{
     va_list args;
     int count = 0;
 
     va_start(args, format);
 
-    while (*format) {
-        if (*format == '%') {
-            // Handle format specifier
-            format++;
-            switch (*format) {
-                case 'c':
-                    // Print a character
-                    putchar(va_arg(args, int));
-                    count++;
-                    break;
-                case 's':
-                    // Print a string
-                    {
-                        const char *str = va_arg(args, const char *);
-                        while (*str) {
-                            putchar(*str);
-                            str++;
-                            count++;
-                        }
-                    }
-                    break;
-                case '%':
-                    // Print a percent sign
-                    putchar('%');
-                    count++;
-                    break;
-                default:
-                    // Invalid format specifier, just print it
-                    putchar('%');
-                    putchar(*format);
-                    count += 2;
+    while (*format)
+    {
+        if (*format == '%' && *(format + 1) != '\0')
+        {
+            format++; /* Move to the next character after '%' */
+            switch (*format)
+            {
+            case 'c':
+                count += _putchar(va_arg(args, int));
+                break;
+            case 's':
+                count += _puts(va_arg(args, char *));
+                break;
+            case '%':
+                count += _putchar('%');
+                break;
+            default:
+                count += _putchar('%');
+                count += _putchar(*format);
+                break;
             }
-        } else {
-            // Regular character, print it
-            putchar(*format);
-            count++;
+        }
+        else
+        {
+            count += _putchar(*format);
         }
 
-        // Move to the next character in the format string
         format++;
     }
 
     va_end(args);
 
     return count;
-}
-
-int main() {
-    _printf("Hello, %s! The answer is %d%c\n", "world", 42, '%');
-    return 0;
 }
